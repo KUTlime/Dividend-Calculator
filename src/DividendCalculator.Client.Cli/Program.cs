41346d4014d74648374703d725a062c153625ea6
KUTlime <kutlime@gmail.com>
2024-05-27 01:09:02 +0200

    Add service registration

--- src/DividendCalculator.Client.Cli/Program.cs
+++ src/DividendCalculator.Client.Cli/Program.cs
@@ -1,3 +1,9 @@
 ﻿var builder = CoconaApp.CreateBuilder();
+
+builder.Services.AddTransient<IDividendCalendarParser<string>, JsonDividendCalendarParser>();
+builder.Services.AddTransient<IDividendParser<string>, JsonDividendPaymentParser>();
+builder.Services.AddTransient<IOrdersParser<string>, JsonTradeOrdersParser>();
+
 var app = builder.Build();
-await app.RunAsync();
+
+await app.RunAsync<ProcessDividendCommands>();
