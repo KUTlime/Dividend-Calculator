15d1bc337c25f4d3ec8544c644c2bcfb5a4a8fa6
KUTlime <kutlime@gmail.com>
2024-05-27 11:12:29 +0200

    Add extension method for filtering

---
+++ src/DividendCalculator.Business/StockTradeOrderExtensions.cs
@@ -0,0 +1,6 @@
+﻿namespace DividendCalculator.Business;
+
+internal static class StockTradeOrderExtensions
+{
+    internal static bool IsTheSameStock(this StockTradeOrder tradeOrder, Stock stock) => string.CompareOrdinal(tradeOrder.Stock.Name, stock.Name) is 0;
+}
